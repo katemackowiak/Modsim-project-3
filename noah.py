949c837b330dcedb14872d0aef1a7fbd635c0e80
Kate Mackowiak <36133267+katemackowiak@users.noreply.github.com>
2019-12-05 20:19:43 -0500

    Graphing works really nicely now

--- noah.py
+++ noah.py
@@ -18,7 +18,7 @@ def make_system(omega):
 
 
     t_0 = 0 * s
-    t_end = 30 * s
+    t_end = 2 * pi / omega 
     dt = t_end/50
 
     g = 9.81 *m*(s**-2)
@@ -38,7 +38,7 @@ def slope_func(state, t, system):
 
     V = Vector((-1*(unit_pos.y) *  omega * r), (unit_pos.x * omega * r)) 
  
-    f_c = Vector(mass * V[0]**2  / r, mass *V[1]**2  / r)
+    f_c = Vector(unit_pos.x * mass * (V.mag)**2  / r, unit_pos.y * mass *(V.mag)**2  / r)
     f_g = Vector(0*kg*m*s**-2, -1*mass * g)
     
     f_t = f_c + f_g
@@ -49,8 +49,15 @@ def slope_func(state, t, system):
         if launched == False:
             launched = True
             print('he has launched at the speed of {} or {}'.format(omega, V.mag))
-        
+
+    global radians
+    global speed_step
+    global graph_speed
+    radians += omega * dt /2   
+    graph_speed += speed_step*((omega*dt/2)%(2*pi)) / (2*pi)
     force_array.append(force)
+    radians_array.append(radians)
+    speed_array.append(graph_speed)
 
     dposdt = [V] 
 
@@ -61,24 +68,33 @@ def event_func(state, t, system):
     pos[1] = pos[1] - system.r
     return pos
 
-force_array = []
-speed = 0.7000000000000004/s
-speed_step = .025/s
-speed_end = 0.7100000000000004/s
+max_val = []
+min_val = []
+radians = 0
+init_speed = .1/s
+speed = init_speed #dont forget units
+graph_speed = speed 
+speed_step = .025/s #dont forget units
+speed_end = 1/s   #dont forget units
 system = make_system(speed)
 launched = False
 while speed < speed_end:
+    force_array = []
+    radians_array = []
     results, details = run_ode_solver(system, slope_func)
     speed += speed_step
     system = make_system(speed)
+    label = 'current speed: ' + str(speed)
+    max_val.append(np.amax(force_array))
+    min_val.append(np.amin(force_array))
+    plot(radians_array, force_array, label = label)
+
 
 time = linspace(0,len(force_array)*system.dt/2,len(force_array))
-
-plot(time, force_array, label = 'Force to Hang On')
-plot([0,time[-1]],[system.grip,system.grip], label = 'Grip Strength')
-plot
-decorate(xlabel='Time (Seconds)',
+plot(speeds,max_val, label = 'max force per rotation at given speed')
+plot(speeds,min_val, label = 'min force per rotation at given speed')
+decorate(xlabel='Angular Speed (Rad/Sec)',
          ylabel='Force (Newtons)',
-         title='Force Required Over Time to Not Fall Off the Ferris Wheel',
+         title='Max and Min Forces Per Rotation of Increasing Speed Values',
          legend=True)
 plt.show()
